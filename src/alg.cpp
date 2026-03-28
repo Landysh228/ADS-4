6c8e3b24a616b07aceecfc10c52b367a64e2f6a7
Landysh <walfa45milana@mail.ru>
2026-03-28 11:40:06 +0300

    Чжен-25ИСТ41.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,19 @@
 #include "alg.h"
 
 int countPairs1(int *arr, int len, int value) {
- int per1 = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                per1++;
-                while (j + 1 < len && arr[j] == arr[j + 1]) {
-                    j++;
-                }
-            }
-        }
-        while (i + 1 < len && arr[i] == arr[i + 1]) {
-            i++;
-        }
+  int per1 = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        per1++;
+      }
     }
-    return per1;
+  }
+  return per1;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int perem = 0;
+  int per2 = 0;
   int left = 0;
   int right = len - 1;
   while (left < right) {
@@ -30,7 +24,7 @@ int countPairs2(int *arr, int len, int value) {
     if (sum == value) {
       if (arr[left] == arr[right]) {
         int x = right - left + 1;
-        perem += x * (x - 1) / 2;
+        per2 += x * (x - 1) / 2;
         break;
       }
       int leftPerem = 1;
@@ -39,7 +33,7 @@ int countPairs2(int *arr, int len, int value) {
         leftPerem++;
       while (right - rightPerem > left && arr[right] == arr[right - rightCount])
         rightPerem++;
-          perem += leftPerem * rightPerem;
+          per2 += leftPerem * rightPerem;
       left += leftPerem;
       right -= rightPerem;
     } else if (sum < value) {
@@ -48,25 +42,25 @@ int countPairs2(int *arr, int len, int value) {
       right--;
     }
   }
-  return perem;
+  return per2;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int perem = 0;
+  int per3 = 0;
   for (int i = 0; i < len; i++) {
     if (i > 0 && arr[i] == arr[i - 1])
       continue;
     int tar = value - arr[i];
     if (tar < arr[i])
       break;
-    int low = -1;
-    int hig = -1;
+    int lows = -1;
+    int highs = -1;
     int left = i + 1;
     int right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == tar) {
-        lo = mid;
+        lows = mid;
         right = mid - 1;
       } else if (arr[mid] < tar) {
         left = mid + 1;
@@ -74,9 +68,9 @@ int countPairs3(int *arr, int len, int value) {
         right = mid - 1;
       }
     }
-    if (low == -1)
+    if (lows == -1)
       continue;
-    left = low;
+    left = lows;
     right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
@@ -91,13 +85,13 @@ int countPairs3(int *arr, int len, int value) {
     }
     if (arr[i] == tar) {
         int n = hi - i + 1;
-      perem += n * (n - 1) / 2;
+      per3 += n * (n - 1) / 2;
       break;
       }
     int leftPerem = 1;
     while (i + leftPerem < len && arr[i + leftPerem] == arr[i])
       leftPerem++;
-    perem += leftPerem * (hi - lo + 1);
+    per3 += leftPerem * (hi - lo + 1);
   }
-  return perem;
+  return per3;
 }
